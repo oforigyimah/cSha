ebd4f9cafa3ac9f9a1c506125b29df43a040f712
Rabbi Ofori Gyimah <rabbioforigyimah@gmail.com>
2024-01-10 15:51:32 +0000

    update

---
+++ get_mt.c
@@ -0,0 +1,5 @@
+//
+// Created by f0rio2 on 1/10/24.
+//
+
+#include "get_mt.h"
---
+++ get_mt.h
@@ -0,0 +1,8 @@
+//
+// Created by f0rio2 on 1/10/24.
+//
+
+#ifndef CSHA_GET_MT_H
+#define CSHA_GET_MT_H
+
+#endif //CSHA_GET_MT_H
